5cc6d21af0288bcf28b6e276f03eaf7195ab3052
unknown <demitri@mit.edu>
2013-11-07 11:41:07 -0500

    big commit, added data structure and currently testing

---
+++ src/minesweeperclass/MinesweeperBoard.java
@@ -0,0 +1,160 @@
+package minesweeperclass;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MinesweeperBoard {
+	private final char[][] Board;
+	private final char BOMB = 'B';
+	private final char UNTOUCHED = '-';
+	private final char DUG = 'D';
+	private final char FLAGGED = 'F';
+	private final char FLAGGED_BOMB = 'C';
+	private final int size;
+
+	public MinesweeperBoard(int size) throws IllegalArgumentException {
+		if (size < 1) {
+			throw new IllegalArgumentException(
+					"Invalid size input! We need at least a 1x1 grid to play Minesweeper");
+		}
+		this.size = size;
+		Board = new char[size][size];
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				if (Math.random() < .25) {
+					Board[i][j] = BOMB;
+				} else {
+					Board[i][j] = UNTOUCHED;
+				}
+			}
+		}
+	}
+
+	public MinesweeperBoard(char[][] board) throws IllegalArgumentException {
+		if (board.length != board[0].length) {
+			throw new IllegalArgumentException(
+					"You input a grid that was not a square!");
+		} else if (board.length < 1) {
+			throw new IllegalArgumentException(
+					"We need at least a 1x1 grid to play Minesweeper");
+		}
+		size = board.length;
+		Board = board;
+		if (!checkRep()) {
+			throw new IllegalArgumentException("Input an invalid grid");
+		}
+	}
+
+	public MinesweeperBoard() {
+		this(10);
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public boolean checkRep() {
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				if (!(isType(i, j, BOMB, UNTOUCHED)
+						|| isType(i, j, DUG, FLAGGED) || isType(i, j,
+							FLAGGED_BOMB))) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	private boolean isType(int x, int y, char type) {
+		if ((x < size && x >= 0) && (y < size && y >= 0)) {
+			return Board[y][x] == type;
+		}
+		return false;
+	}
+
+	private boolean isType(int x, int y, char type1, char type2) {
+		return isType(x, y, type1) || isType(x, y, type2);
+	}
+
+	private String getBombNeighbors(int x, int y) {
+		int bombCount = 0;
+		for (int i = -1; i < 2; i++) {
+			for (int j = -1; j < 2; j++) {
+				if (!(i == 0 && j == 0)
+						&& isType(x + i, y + j, BOMB, FLAGGED_BOMB)) {
+					bombCount++;
+				}
+			}
+		}
+		if (bombCount == 0) {
+			return " ";
+		}
+		return "" + bombCount;
+	}
+
+	private void setDug(int x, int y) {
+		if (getBombNeighbors(x, y).equals(" ")) {
+			for (int i = -1; i < 2; i++) {
+				for (int j = -1; j < 2; j++) {
+					if (!(i == 0 && j == 0) && isType(x + i, y + j, UNTOUCHED)) {
+						setDug(x + i, y + j);
+					}
+				}
+			}
+		}
+		Board[y][x] = DUG;
+	}
+
+	private List<String> getBoard() {
+		List<String> boardStrings = new ArrayList<String>();
+		for (int i = 0; i < size; i++) {
+			boardStrings.add("");
+			for (int j = 0; j < size; j++) {
+				if (isType(j, i, DUG)) {
+					boardStrings.set(i,
+							boardStrings.get(i) + getBombNeighbors(j, i) + " ");
+				} else if (isType(j, i, FLAGGED, FLAGGED_BOMB)) {
+					boardStrings.set(i, boardStrings.get(i) + "F" + " ");
+				} else if (isType(j, i, UNTOUCHED, BOMB)) {
+					boardStrings.set(i, boardStrings.get(i) + "-" + " ");
+				} else {
+					throw new RuntimeException(
+							"There was an undefined type in the MinesweeperBoard");
+				}
+
+			}
+		}
+		return boardStrings;
+	}
+
+	public synchronized List<String> flag(int x, int y) {
+		if (isType(x, y, BOMB, UNTOUCHED)) {
+			Board[y][x] = FLAGGED;
+		}
+		return getBoard();
+	}
+
+	public synchronized List<String> deFlag(int x, int y) {
+		if (isType(x, y, FLAGGED)) {
+			Board[y][x] = UNTOUCHED;
+		}
+		return getBoard();
+	}
+
+	public synchronized List<String> dig(int x, int y) {
+		if (isType(x, y, UNTOUCHED)) {
+			setDug(x, y);
+		} else if (isType(x, y, BOMB)) {
+			Board[y][x] = DUG;
+			// indicates socket should be closed
+			return null;
+		}
+		return getBoard();
+	}
+
+	public synchronized List<String> look() {
+		return getBoard();
+	}
+
+}
---
+++ src/minesweeperclass/MinesweeperBoardTest.java
@@ -0,0 +1,259 @@
+package minesweeperclass;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class MinesweeperBoardTest {
+
+	/*
+	 * Test each public method, testing valid grid inputs and invalid grid
+	 * inputs
+	 * 
+	 * Testing space: constructor(), constructor(int size), constructor(char[][]
+	 * board), flag(x,y), deFlag(x,y), dig(x,y), recursive chaining from
+	 * dig(x,y), look(), checkRep()
+	 */
+
+	/*
+	 * The following tests will test the constructors with valid and invalid
+	 * inputs. If the input is invalid, test for IllegalArgumentException
+	 */
+	@Test
+	public void testDefaultConstructor() {
+		// default constructor test
+		MinesweeperBoard board = new MinesweeperBoard();
+		assertTrue(board.look().size() == 10);
+		assertTrue(board.checkRep());
+	}
+
+	@Test
+	public void testSizeConstructor() {
+		// constructor with size input test
+		MinesweeperBoard board = new MinesweeperBoard(20);
+		assertTrue(board.look().size() == 20);
+		assertTrue(board.checkRep());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSizeConstructorException() {
+		// test invalid size arguments for exception
+		new MinesweeperBoard(-1);
+		new MinesweeperBoard(0);
+	}
+
+	@Test
+	public void testBoardConstructor() {
+		// input char[][] constructor test
+		char[][] testBoard = new char[5][5];
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (j % 2 == 1) {
+					testBoard[i][j] = 'B';
+				} else
+					testBoard[i][j] = '-';
+			}
+		}
+		// construct expected List<String>
+		MinesweeperBoard board = new MinesweeperBoard(testBoard);
+		List<String> expectedStringArray = new ArrayList<String>();
+		for (int i = 0; i < 5; i++) {
+			expectedStringArray.add("");
+			for (int j = 0; j < 5; j++) {
+				expectedStringArray.set(i, expectedStringArray.get(i) + "- ");
+			}
+		}
+		// compare look() to the expected List<String>
+		assertTrue(board.look().equals(expectedStringArray));
+		assertTrue(board.checkRep());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testBoardConstructorException() {
+		// test invalid char[][] inputs for exception
+		new MinesweeperBoard(new char[2][3]);
+		new MinesweeperBoard(new char[3][2]);
+		new MinesweeperBoard(new char[0][0]);
+	}
+
+	/*
+	 * The following tests will focus on flag(x,y) and deFlag(x,y) testing for
+	 * actions if given valid x,y grid coordinates and handling for invalid x,y
+	 * coordinates (taking no action)
+	 */
+
+	@Test
+	public void testFlagValidInputs() {
+		// test valid inputs for board.flag(x,y)
+		char[][] testBoard = new char[5][5];
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (j % 2 == 1) {
+					testBoard[i][j] = 'B';
+				} else
+					testBoard[i][j] = '-';
+			}
+		}
+		// construct expected List<String>
+		MinesweeperBoard board = new MinesweeperBoard(testBoard);
+		List<String> expectedStringArray = new ArrayList<String>();
+		for (int i = 0; i < 5; i++) {
+			expectedStringArray.add("");
+			for (int j = 0; j < 5; j++) {
+				expectedStringArray.set(i, expectedStringArray.get(i) + "- ");
+			}
+		}
+		// flag 2,2
+		List<String> boardRep = board.flag(2, 2);
+		expectedStringArray.set(2, "- - F - - ");
+		assertTrue(boardRep.equals(expectedStringArray));
+		assertTrue(board.checkRep());
+
+		// now with the same board, flag 3,2
+		boardRep = board.flag(3, 2);
+		expectedStringArray.set(2, "- - F F - ");
+		assertTrue(boardRep.equals(expectedStringArray));
+		assertTrue(board.checkRep());
+
+		// dig at 0,0 then flag it
+		board.dig(0, 0);
+		boardRep = board.flag(0, 0);
+		for (int i = 0; i < 5; i++) {
+			if (i == 0) {
+				expectedStringArray.set(i, "2 - - - - ");
+			} else if (i == 2) {
+				expectedStringArray.set(i, "- - F F - ");
+
+			} else {
+				expectedStringArray.set(i, "- - - - - ");
+			}
+		}
+		// check that it did not mark the exposed spot as FLAGGED
+		assertTrue(boardRep.equals(expectedStringArray));
+		assertTrue(board.checkRep());
+	}
+
+	@Test
+	public void testFlagInvalidInputs() {
+		// test for inputs that are not on the grid for board.flag(x,y)
+
+		// construct testBoard
+		char[][] testBoard = new char[5][5];
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (j % 2 == 1) {
+					testBoard[i][j] = 'B';
+				} else
+					testBoard[i][j] = '-';
+			}
+		}
+		// construct expected List<String>
+		MinesweeperBoard board = new MinesweeperBoard(testBoard);
+		List<String> expectedStringArray = new ArrayList<String>();
+		for (int i = 0; i < 5; i++) {
+			expectedStringArray.add("");
+			for (int j = 0; j < 5; j++) {
+				expectedStringArray.set(i, expectedStringArray.get(i) + "- ");
+			}
+		}
+		// incorrect grid positions
+		board.flag(-1, 0);
+		board.flag(0, -1);
+		board.flag(6, 4);
+		board.flag(4, 6);
+		// check that nothing was set to FLAGGED
+		assertTrue(board.look().equals(expectedStringArray));
+		assertTrue(board.checkRep());
+	}
+
+	@Test
+	public void testDeFlagValidInputs() {
+		// test deFlag(x,y) for (x,y) that are on the grid
+
+		// construct testBoard
+		char[][] testBoard = new char[5][5];
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (j % 2 == 1) {
+					testBoard[i][j] = 'B';
+				} else
+					testBoard[i][j] = '-';
+			}
+		}
+		// construct expected List<String>
+		MinesweeperBoard board = new MinesweeperBoard(testBoard);
+		List<String> expectedStringArray = new ArrayList<String>();
+		for (int i = 0; i < 5; i++) {
+			expectedStringArray.add("");
+			for (int j = 0; j < 5; j++) {
+				expectedStringArray.set(i, expectedStringArray.get(i) + "- ");
+			}
+		}
+		// insert a flag and deflag it. check that there are no flags on the
+		// board
+		board.flag(0, 0);
+		List<String> boardRep = board.deFlag(0, 0);
+		assertTrue(boardRep.equals(expectedStringArray));
+		assertTrue(board.checkRep());
+
+		// insert lots of flags and deflag them. check that there are no flags
+		// on the board
+		board.flag(0, 0);
+		board.flag(1, 2);
+		board.flag(3, 2);
+		board.flag(4, 3);
+		board.deFlag(3, 2);
+		board.deFlag(1, 2);
+		board.deFlag(4, 3);
+		boardRep = board.deFlag(0, 0);
+		assertTrue(boardRep.equals(expectedStringArray));
+		assertTrue(board.checkRep());
+
+	}
+
+	@Test
+	public void testDeFlagOtherInputs() {
+		// test all other possible deFlag(x,y) situations
+
+		// construct testBoard
+		char[][] testBoard = new char[5][5];
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (j % 2 == 1) {
+					testBoard[i][j] = 'B';
+				} else
+					testBoard[i][j] = '-';
+			}
+		}
+		MinesweeperBoard board = new MinesweeperBoard(testBoard);
+		List<String> expectedStringArray = new ArrayList<String>();
+		for (int i = 0; i < 5; i++) {
+			expectedStringArray.add("");
+			for (int j = 0; j < 5; j++) {
+				expectedStringArray.set(i, expectedStringArray.get(i) + "- ");
+			}
+		}
+		// test on untouched (x,y)
+		List<String> boardRep = board.deFlag(2, 2);
+		assertTrue(boardRep.equals(expectedStringArray));
+		assertTrue(board.checkRep());
+
+		// test outside of bounds
+		board.deFlag(-1, 0);
+		board.deFlag(0, -1);
+		board.deFlag(6, 3);
+		boardRep = board.deFlag(3, 6);
+		assertTrue(boardRep.equals(expectedStringArray));
+		assertTrue(board.checkRep());
+
+		// test on exposed (x,y)
+		board.dig(0, 0);
+		expectedStringArray.set(0, "2 - - - - ");
+		boardRep = board.deFlag(0, 0);
+		assertTrue(boardRep.equals(expectedStringArray));
+		assertTrue(board.checkRep());
+	}
+}
